8f78e224408f8a26fe3eb85ccfe168f35d353ec4
abdallahawad3 <djdgdnnxcbj@gmail.com>
2025-01-26 21:26:52 +0200

    Implement theme provider and add dark mode support with updated CSS variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
+import { ThemeProvider } from "./providers/theme-provider";
+import { ModeToggle } from "@/components/ThemeChanger";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +26,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange>
+          <ModeToggle />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
