b74862345d37a9953ed27e4e34be1adf2f6584b1
abdallahawad3 <djdgdnnxcbj@gmail.com>
2025-01-29 17:34:46 +0200

    hh

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,12 @@ import TodoTable from "@/components/TodoTable";
 import { auth } from "@clerk/nextjs/server";
 
 export default async function Home() {
+  const start = Date.now();
   const { userId } = await auth();
   const todos = await getTodosAction({ id: userId });
+  const end = Date.now();
 
+  console.log("Time::::: ", (end - start) / 1000);
   return (
     <section className="container mt-16">
       <div className="flex justify-end w-full mb-2">
